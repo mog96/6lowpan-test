9393eff1bf7008da21237e260dbd2b7b501562fd
Mateo Garcia <mateog@stanford.edu>
2017-07-03 15:58:59 -0700

    Adds spi_test, uses python periphery SPI library.

---
+++ spi_test.py
@@ -0,0 +1,11 @@
+from periphery import SPI
+
+while True:
+  # Open spidev1.0 with mode 0 and max speed 1MHz
+  spi = SPI("/dev/spidev0.0", 0, 400000)
+
+  data_out = [0x00, 0x00, 0x00, 0x00]
+  data_in = spi.transfer(data_out)
+
+  print("shifted out [0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_out))
+  print("shifted in  [0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_in))
