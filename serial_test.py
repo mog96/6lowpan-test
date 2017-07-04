e8e5483b60e6195edc51155321ef13d31a981065
Mateo Garcia <mateog@stanford.edu>
2017-07-04 10:59:45 -0700

    Finessiiiiin finessin.

--- serial_test.py
+++ serial_test.py
@@ -2,6 +2,7 @@
 # get lines of text from serial port, save them to a file
 
 import serial
+import sys
 
 addr  = '/dev/ttyUSB0'  # serial port to read data from
 baud  = 128000          # baud rate for serial port
@@ -9,9 +10,8 @@ fname = 'log.txt'       # log file to save data in
 fmode = 'a'             # log file mode = append
 
 with serial.Serial(addr, baud) as ser, open(fname, fmode) as f:
-    ser.readline()  # throw away first line; likely to start mid-sentence (incomplete)
     while (1):
-        x = ser.read()  # read one line of text from serial port
-        print x,        # echo byte on-screen as ASCII
-        f.write(x)      # write line of text to file
-        f.flush()       # make sure it actually gets written out
+        x = ser.read()         # read one line of text from serial port
+        sys.stdout.write('x')  # echo byte on-screen as ASCII
+        f.write(x)             # write line of text to file
+        f.flush()              # make sure it actually gets written out
