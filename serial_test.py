35062be72ca8f505439f44032e42e516231a023e
Mateo Garcia <mateog@stanford.edu>
2017-07-04 11:08:31 -0700

    Updates log file naming scheme to include current time, and stores in 'logs' folder.

--- serial_test.py
+++ serial_test.py
@@ -4,10 +4,10 @@
 import serial
 import sys
 
-addr  = '/dev/ttyUSB0'  # serial port to read data from
-baud  = 128000          # baud rate for serial port
-fname = 'log.txt'       # log file to save data in
-fmode = 'a'             # log file mode = append
+addr  = '/dev/ttyUSB0'                                 # serial port to read data from
+baud  = 128000                                         # baud rate for serial port
+fname = 'logs/log' + datetime.datetime.now() + '.txt'  # log file to save data in
+fmode = 'a'                                            # log file mode = APPEND
 
 with serial.Serial(addr, baud) as ser, open(fname, fmode) as f:
     while (1):
