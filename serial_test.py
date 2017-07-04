956365c582319fe04a47dc642765315e4b59e8f4
Mateo Garcia <mateog@stanford.edu>
2017-07-04 10:56:44 -0700

    She runs! I can read from serial and log it. Bout damn time.

--- serial_test.py
+++ serial_test.py
@@ -12,6 +12,6 @@ with serial.Serial(addr, baud) as ser, open(fname, fmode) as f:
     ser.readline()  # throw away first line; likely to start mid-sentence (incomplete)
     while (1):
         x = ser.read()  # read one line of text from serial port
-        print(x)    # echo line of text on-screen
-        f.write(x)       # write line of text to file
-        f.flush()        # make sure it actually gets written out
+        print x,        # echo byte on-screen as ASCII
+        f.write(x)      # write line of text to file
+        f.flush()       # make sure it actually gets written out
