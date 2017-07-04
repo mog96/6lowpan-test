e4fe1e0d21d9e7a1a8396c341f35fc2ff5e95c90
Mateo Garcia <mateog@stanford.edu>
2017-07-04 11:11:48 -0700

    Open as write.

--- serial_test.py
+++ serial_test.py
@@ -9,7 +9,7 @@ addr  = '/dev/ttyUSB0'                   # serial port to read data from
 baud  = 128000                           # baud rate for serial port
 date  = datetime.datetime.now()
 fname = 'logs/log' + str(date) + '.txt'  # log file to save data in
-fmode = 'a'                              # log file mode = APPEND
+fmode = 'w'                              # log file mode = APPEND
 
 with serial.Serial(addr, baud) as ser, open(fname, fmode) as f:
     while (1):
