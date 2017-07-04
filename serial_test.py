2ea52dbc8701ac6aa0db8f2324b0dc02590059c5
Mateo Garcia <mateog@stanford.edu>
2017-07-03 20:37:43 -0700

    Adjusts baud.

--- serial_test.py
+++ serial_test.py
@@ -5,11 +5,11 @@ from __future__ import print_function
 import serial, io
 
 addr  = '/dev/ttyUSB0'  # serial port to read data from
-baud  = 9600            # baud rate for serial port
+baud  = 115200          # baud rate for serial port
 fname = 'log.txt'       # log file to save data in
 fmode = 'a'             # log file mode = append
 
-with serial.Serial(addr,9600) as pt, open(fname,fmode) as outf:
+with serial.Serial(addr, baud) as pt, open(fname,fmode) as outf:
     spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
         encoding='ascii', errors='ignore', newline='\r',line_buffering=True)
     spb.readline()  # throw away first line; likely to start mid-sentence (incomplete)
