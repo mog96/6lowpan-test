69c0157a1caf87156a63784f58e06067e7359516
Mateo Garcia <mateog@stanford.edu>
2017-07-03 19:43:32 -0700

    Writes to log file.

--- spi_test.py
+++ spi_test.py
@@ -1,14 +1,15 @@
 from periphery import SPI
 from time import sleep
 
-while True:
-  # Open spidev1.0 with mode 0 and max speed 1MHz
-  spi = SPI("/dev/spidev0.0", 0, 400000)
+with open("log.txt", 'w') as f:
+  while True:
+    # Open spidev1.0 with mode 0 and max speed 1MHz
+    spi = SPI("/dev/spidev0.0", 0, 400000)
 
-  data_out = [0x00, 0x00, 0x00, 0x00]
-  data_in = spi.transfer(data_out)
+    data_out = [0x00, 0x00, 0x00, 0x00]
+    data_in = spi.transfer(data_out)
 
-  print("shifted out [0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_out))
-  print("shifted in  [0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_in))
+    f.write("[0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_out))
+    f.write("[0x%02x, 0x%02x, 0x%02x, 0x%02x]" % tuple(data_in))
 
-  # sleep(1/400000)
+    # sleep(1/400000)
