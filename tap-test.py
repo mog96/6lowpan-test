7bb84abab709222756977ad8af8d55471f734987
Mateo Garcia <mateog@stanford.edu>
2017-07-05 14:36:38 -0700

    More tests

--- tap-test.py
+++ tap-test.py
@@ -1,11 +1,6 @@
 from pytun import TunTapDevice, IFF_TAP
 
 tap = TunTapDevice(name='tap0', flags=IFF_TAP)
-print tap.name
-print tap.addr
-print tap.dstaddr
-print tap.netmask
-print tap.mtu
 
 while 1:
   print "swag",
