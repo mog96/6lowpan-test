f2822969e97fd388073ae653065f35936ec255cd
Mateo Garcia <mateog@stanford.edu>
2017-07-05 14:54:00 -0700

    Setting all params.

--- tap-test.py
+++ tap-test.py
@@ -1,6 +1,9 @@
 from pytun import TunTapDevice, IFF_TAP
 
 tap = TunTapDevice(name='tap0', flags=IFF_TAP)
+tun.addr = '10.8.0.1'
+tun.dstaddr = '10.8.0.2'
+tun.netmask = '255.255.255.0'
 tap.mtu = 1280
 
 while 1:
