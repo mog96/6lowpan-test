2b2ac84ed98145e94d1b9c82f20b6bd9be483d79
Mateo Garcia <mateog@stanford.edu>
2017-07-06 10:02:09 -0700

    Swag?

--- tap_write.py
+++ tap_write.py
@@ -3,5 +3,5 @@ from time import sleep
 
 while 1:
   s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-  s.sendto('helloworld', ('10.8.0.1' ,12345))
+  s.sendto('helloworld', '10.8.0.1')
   sleep(2)
