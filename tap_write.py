4522df08bdedc3470efe9f8b5102a00bff0001a8
Mateo Garcia <mateog@stanford.edu>
2017-07-06 11:05:52 -0700

    Writes to tap addr instead of dst addr

--- tap_write.py
+++ tap_write.py
@@ -4,5 +4,5 @@ from time import sleep
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while 1:
-  s.sendto('helloworld', ('10.8.0.2', 12345))
+  s.sendto('helloworld', ('10.8.0.1', 12345))
   sleep(2)
