3d7f0cdaf97b6c5593f698bf0c8ad334615a627f
Mateo Garcia <mateog@stanford.edu>
2017-07-06 10:19:09 -0700

    Raw unsupported.

--- tap_write.py
+++ tap_write.py
@@ -2,6 +2,6 @@ import socket
 from time import sleep
 
 while 1:
-  s = socket.socket(socket.AF_INET,socket.SOCK_RAW)
+  s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
   s.sendto('helloworld', ('10.8.0.2', 12345))
   sleep(2)
