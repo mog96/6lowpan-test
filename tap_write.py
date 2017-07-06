085074833cce5bc802eb977f8a2a037e3248d236
Mateo Garcia <mateog@stanford.edu>
2017-07-06 10:33:53 -0700

    Was creating socket on every loop...

--- tap_write.py
+++ tap_write.py
@@ -1,7 +1,8 @@
 import socket
 from time import sleep
 
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
 while 1:
-  s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
   s.sendto('helloworld'.encode('utf-8'), ('10.8.0.2', 12345))
   sleep(2)
