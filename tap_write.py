25c30b8ce9fa32858fbf9e32cfcc55f1ea4cdd06
Mateo Garcia <mateog@stanford.edu>
2017-07-06 10:27:59 -0700

    Only decode.

--- tap_write.py
+++ tap_write.py
@@ -3,5 +3,5 @@ from time import sleep
 
 while 1:
   s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-  s.sendto('helloworld'.encode('utf-8'), ('10.8.0.2', 12345))
+  s.sendto('helloworld', ('10.8.0.2', 12345))
   sleep(2)
