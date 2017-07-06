965d6600baaffc7dade3698af6f4b9e97e82a177
Mateo Garcia <mateog@stanford.edu>
2017-07-06 10:59:21 -0700

    Attempts to use ipv6 address as tap address.

--- tap-test.py
+++ tap-test.py
@@ -2,7 +2,7 @@ from pytun import TunTapDevice, IFF_TAP
 from time import sleep
 
 tap = TunTapDevice(name='tap0', flags=IFF_TAP)
-tap.addr = '10.8.0.1'
+tap.addr = 'fa:ca:a9:29:69:61:65:3b' # '10.8.0.1'
 tap.dstaddr = '10.8.0.2'
 tap.netmask = '255.255.255.0'
 tap.mtu = 1280
