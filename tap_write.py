f6d3bd8894080c39029b5b582aef78d21149cec6
Mateo Garcia <mateog@stanford.edu>
2017-07-07 11:27:09 -0700

    Testing socket connect to wpantund

--- tap_write.py
+++ tap_write.py
@@ -4,5 +4,5 @@ from time import sleep
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while 1:
-  s.sendto('helloworld', ('10.8.0.2', 12345))
+  s.sendto('helloworld', ('127.0.0.1', 4901))
   sleep(2)
